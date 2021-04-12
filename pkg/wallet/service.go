a0d7396adb51b5460ea729596a591cc955035533
muhammad6661 <nurmatov.muhammad000@gmail.com>
2021-04-13 03:25:17 +0500

    change Fix v1.1.0

--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -102,9 +102,13 @@ func (s *Service)Reject(paymentID string ) error{
    for _,i:=range s.payments{
      if(i.ID==paymentID){
        i.Status=types.PaymentStatusFail
-       account,_:=s.FindAccountByID(i.AccountID)
-       account.Balance+=i.Amount
-       i.Amount=0
+       for _,j:=range s.accounts{
+           if j.ID==i.AccountID{
+             j.Balance+=i.Amount
+            i.Amount=0
+           }
+       }
+     
        return nil
      }
    }
@@ -125,6 +129,7 @@ func (service *Service) FindAccountByID(AccountID int64)(*types.Account,error){
 	
    for _,account:=range service.accounts{
 	   if(account.ID==AccountID){
+		   account.Balance=0
 		   return account,nil
 	   }
    }
