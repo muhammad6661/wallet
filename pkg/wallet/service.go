096dc748235c5873130a02c442e1d2f0a98a05ae
muhammad6661 <nurmatov.muhammad000@gmail.com>
2021-04-18 15:38:23 +0500

    new F

--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -234,7 +234,7 @@ func (s *Service)FindFavoriteByID(favoriteID string) (*types.Favorites,error){
 
 func (s *Service)ExportToFile(path string) error{
     
-  file,err:=os.Create(path)
+  file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
   if err!=nil{
     return err
   }
